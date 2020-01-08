d78d013e3dbf4ac870779a93fe328f724b6ec5ae
강수진 <rkdthd1234@naver.com>
2020-01-08 12:45:12 +0900

    initial project

---
+++ SwiftUIPractice/SwiftUIPractice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIPractice
+//
+//  Created by 강수진 on 2020/01/08.
+//  Copyright © 2020 강수진. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
