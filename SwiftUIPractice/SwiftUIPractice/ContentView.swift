d7b0092a4d604aa4a3ba3123256190294b3f831f
강수진 <rkdthd1234@naver.com>
2020-01-08 12:45:51 +0900

    text 다루기

--- SwiftUIPractice/SwiftUIPractice/ContentView.swift
+++ SwiftUIPractice/SwiftUIPractice/ContentView.swift
@@ -9,8 +9,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    static let taskDateFormat: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        return formatter
+    }()
+
+    var dueDate = Date()
+    
     var body: some View {
-        Text("Hello, World!")
+        //text
+        let plainTxt = Text("Hello, World!\nWhat a nice world!What a nice world!What a nice world!What a nice world!")
+            .lineLimit(2)
+            .truncationMode(.middle)
+            .font(.headline)
+            .multilineTextAlignment(.center)
+            .foregroundColor(.green)
+            .lineSpacing(50)
+        let dateTxt = Text("Task due date : \(dueDate, formatter: Self.taskDateFormat)")
+        
+        return plainTxt
     }
 }
 
